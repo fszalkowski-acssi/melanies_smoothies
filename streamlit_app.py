c2a19bfbbee525214448207975dff95e4884d62d
fszalkowski-acssi <fszalkowski@acssi.fr>
2025-11-28 14:43:13 +0100

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ if ingredients:
     for fruit_chosen in ingredients:
         ingredients_str += fruit_chosen + ' '
 
-        search_on = pd_df.loc[pd_df['fruit_name'] == fruit_chosen, 'search_on'].iloc[0]
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
 
         st.subheader(fruit_chosen + ' nutrition information')
